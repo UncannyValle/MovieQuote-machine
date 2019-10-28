27d1dc0a8e6cfbde3c9148b0be489a487e8dd1ca
Julian V <jvalle0926@gmail.com>
2019-10-28 00:19:07 -0700

    working on making a regex

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React from "react";
 import "./App.css";
 import Button from "./components/Button";
+import Text from "./components/Text";
 
 class App extends React.Component {
-  nextQuote() {
-    console.log("hi");
+  constructor(props) {
+    super(props);
+    this.state = {
+      quoteList: [],
+      quote: '',
+      quoteIndex: null
+    };
+    this.chooseQuoteIndex = this.chooseQuoteIndex.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://raw.githubusercontent.com/vilaboim/movie-quotes/e72e64502486d9d9d528277a1dbe94f20f011bc6/movie-quotes.json"
+    )
+      .then(data => data.json())
+      .then(quoteList => this.setState({ quoteList }));
+  }
+
+  chooseQuoteIndex() {
+    this.setState({
+      quoteIndex: Math.floor(Math.random() * this.state.quoteList.length - 1),
+      quote: (this.state.quoteList[this.state.quoteIndex]).search(/\d/)
+    });
+    console.log(this.state.quote);
   }
   render() {
     return (
       <div className="App" id="quote-box">
-        <Button buttonDisplayName={"Hit Me"} clickHandler={this.nextQuote}/>
+        <Text />
+        <Button
+          buttonDisplayName={"Hit Me"}
+          clickHandler={this.chooseQuoteIndex}
+        />
       </div>
     );
   }
---
+++ src/components/Text.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Text = (props) => {
+  return (
+    <div id="text">
+      <p></p>
+    </div>
+  );
+};
+
+export default Text;
